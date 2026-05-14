e8f07b5504760feab36af938aca810426117b055
AngeloGOD <joseangelfigueroa123454321@gmail.com>
2026-05-13 23:30:47 -0600

    Commit final

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO marcas(nombre,pais) VALUES
+('Honda','Japón'),
+('Toyota','Japón'),
+('Nissan','Japón');
+
+INSERT INTO autos VALUES
+('H001','CRV',2024,450000,1),
+('H002','BRV',2023,380000,1),
+('T001','RAV4',2025,520000,2),
+('T999','CAMRY',2025,550000,2),
+('N001','Xtrail',2024,490000,3);
